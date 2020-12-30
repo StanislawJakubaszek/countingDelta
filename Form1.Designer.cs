add0ab4e5e82872ef6cbe67ef2083bcf732d4be3
Stanisław Jakubaszek <st.jakubaszek@onet.pl>
2020-12-30 02:13:21 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace program_projekt
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.checkBoxB = new System.Windows.Forms.CheckBox();
+            this.bEnter = new System.Windows.Forms.Button();
+            this.checkBoxC = new System.Windows.Forms.CheckBox();
+            this.checkBoxA = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(515, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Location = new System.Drawing.Point(93, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AllowDrop = true;
+            this.button2.Location = new System.Drawing.Point(137, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AllowDrop = true;
+            this.button3.Location = new System.Drawing.Point(181, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AllowDrop = true;
+            this.button4.Location = new System.Drawing.Point(225, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(38, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AllowDrop = true;
+            this.button5.Location = new System.Drawing.Point(269, 68);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(38, 38);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AllowDrop = true;
+            this.button6.Location = new System.Drawing.Point(313, 68);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(38, 38);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.AllowDrop = true;
+            this.button7.Location = new System.Drawing.Point(357, 68);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(38, 38);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.AllowDrop = true;
+            this.button8.Location = new System.Drawing.Point(401, 68);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(38, 38);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button0
+            // 
+            this.button0.AllowDrop = true;
+            this.button0.Location = new System.Drawing.Point(489, 68);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(38, 38);
+            this.button0.TabIndex = 10;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // button9
+            // 
+            this.button9.AllowDrop = true;
+            this.button9.Location = new System.Drawing.Point(445, 68);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(38, 38);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // checkBoxB
+            // 
+            this.checkBoxB.AutoSize = true;
+            this.checkBoxB.Location = new System.Drawing.Point(12, 101);
+            this.checkBoxB.Name = "checkBoxB";
+            this.checkBoxB.Size = new System.Drawing.Size(75, 21);
+            this.checkBoxB.TabIndex = 11;
+            this.checkBoxB.Text = "dane B";
+            this.checkBoxB.UseVisualStyleBackColor = true;
+            this.checkBoxB.CheckedChanged += new System.EventHandler(this.checkBoxB_CheckedChanged);
+            // 
+            // bEnter
+            // 
+            this.bEnter.Location = new System.Drawing.Point(189, 112);
+            this.bEnter.Name = "bEnter";
+            this.bEnter.Size = new System.Drawing.Size(338, 51);
+            this.bEnter.TabIndex = 12;
+            this.bEnter.Text = "Enter";
+            this.bEnter.UseVisualStyleBackColor = true;
+            this.bEnter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // checkBoxC
+            // 
+            this.checkBoxC.AutoSize = true;
+            this.checkBoxC.Location = new System.Drawing.Point(12, 128);
+            this.checkBoxC.Name = "checkBoxC";
+            this.checkBoxC.Size = new System.Drawing.Size(75, 21);
+            this.checkBoxC.TabIndex = 13;
+            this.checkBoxC.Text = "dane C";
+            this.checkBoxC.UseVisualStyleBackColor = true;
+            this.checkBoxC.CheckedChanged += new System.EventHandler(this.checkBoxC_CheckedChanged);
+            // 
+            // checkBoxA
+            // 
+            this.checkBoxA.AutoSize = true;
+            this.checkBoxA.Location = new System.Drawing.Point(12, 74);
+            this.checkBoxA.Name = "checkBoxA";
+            this.checkBoxA.Size = new System.Drawing.Size(75, 21);
+            this.checkBoxA.TabIndex = 14;
+            this.checkBoxA.Text = "dane A";
+            this.checkBoxA.UseVisualStyleBackColor = true;
+            this.checkBoxA.CheckedChanged += new System.EventHandler(this.checkBoxA_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(515, 22);
+            this.textBox2.TabIndex = 15;
+            // 
+            // bClear
+            // 
+            this.bClear.Location = new System.Drawing.Point(92, 112);
+            this.bClear.Name = "bClear";
+            this.bClear.Size = new System.Drawing.Size(91, 51);
+            this.bClear.TabIndex = 16;
+            this.bClear.Text = "Clear";
+            this.bClear.UseVisualStyleBackColor = true;
+            this.bClear.Click += new System.EventHandler(this.bClear_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(536, 174);
+            this.Controls.Add(this.bClear);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBoxA);
+            this.Controls.Add(this.checkBoxC);
+            this.Controls.Add(this.bEnter);
+            this.Controls.Add(this.checkBoxB);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form";
+            this.Text = "Obliczator Delty";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.CheckBox checkBoxB;
+        private System.Windows.Forms.Button bEnter;
+        private System.Windows.Forms.CheckBox checkBoxC;
+        private System.Windows.Forms.CheckBox checkBoxA;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button bClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program_projekt
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        string LiczbaA = "A";
+        string LiczbaB = "B";
+        string LiczbaC = "C";
+        char A = 'N';
+        char B = 'N';
+        char C = 'N';
+        public Form()
+        {
+            InitializeComponent();
+            textBox1.Text = LiczbaB + "^2 - 4*" + LiczbaA;
+            textBox1.Text += "*" + LiczbaC;
+            textBox1.Text += " = ";
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = " ";
+            if (LiczbaA != "A" && LiczbaB != "B" && LiczbaC != "C")
+            {
+                textBox1.Text = LiczbaB + "^2 - 4*" + LiczbaA;
+                textBox1.Text += "*" + LiczbaC;
+                textBox1.Text += " = ";
+                textBox1.Text += int.Parse(LiczbaB)*int.Parse(LiczbaB) - 4*int.Parse(LiczbaA)*int.Parse(LiczbaC);
+            }
+            else
+            {
+                textBox2.Text = "Proszę podać wszystkie dane!!!!!!!!!";
+            }
+         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Oblicz(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Oblicz(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Oblicz(4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Oblicz(5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Oblicz(6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Oblicz(7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Oblicz(8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Oblicz(9);
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            Oblicz(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Oblicz(1);
+        }
+
+        private void Oblicz(int liczba)
+        {
+            textBox2.Text = " ";
+            if (A == 'Y' && B!='Y' && C!='Y')
+            {
+                if(LiczbaA=="A")
+                {
+                    LiczbaA = null;
+                }
+                LiczbaA += liczba;
+                textBox1.Text = LiczbaB + "^2 - 4*" + LiczbaA;
+                textBox1.Text += "*" + LiczbaC;
+                textBox1.Text += " = ";
+            }
+            else if (B == 'Y' && A != 'Y' && C != 'Y')
+            {
+                if (LiczbaB == "B")
+                {
+                    LiczbaB = null;
+                }
+                LiczbaB += liczba;
+                textBox1.Text = LiczbaB + "^2 - 4*" + LiczbaA;
+                textBox1.Text += "*" + LiczbaC;
+                textBox1.Text += " = ";
+            }
+            else if (C == 'Y' && B != 'Y' && A != 'Y')
+            {
+                if (LiczbaC == "C")
+                {
+                    LiczbaC = null;
+                }
+                LiczbaC += liczba;
+                textBox1.Text = LiczbaB + "^2 - 4*" + LiczbaA;
+                textBox1.Text += "*" + LiczbaC;
+                textBox1.Text += " = ";
+            }
+            else
+            {
+                textBox2.Text = "Proszę zaznaczyć tylko jedno pole z danymi: A, B lub C!!!!!!!!!!";
+            }
+        }
+
+        private void checkBoxB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (B == 'N')
+            {
+                B = 'Y'; 
+            }
+            else {
+                    B = 'N'; 
+                 }
+        }
+
+        private void checkBoxA_CheckedChanged(object sender, EventArgs e)
+        {
+            if (A == 'N')
+            {
+                A = 'Y';
+            }
+            else
+            {
+                A = 'N';
+            }
+        }
+
+        private void checkBoxC_CheckedChanged(object sender, EventArgs e)
+        {
+            if (C == 'N')
+            {
+                C = 'Y';
+            }
+            else
+            {
+                C = 'N';
+            }
+        }
+        private void bClear_Click(object sender, EventArgs e)
+        {
+            LiczbaA = "A";
+            LiczbaB = "B";
+            LiczbaC = "C";
+            textBox1.Text = LiczbaB + "^2 - 4*" + LiczbaA;
+            textBox1.Text += "*" + LiczbaC;
+            textBox1.Text += " = ";
+            textBox2.Text = "Dane wyczyszczono";
+        }
+
+    }
+}
